707ce01f78f7dfbf58b131c72545c59f00db4078
Josh Worley <helloworley@gmail.com>
2022-03-20 20:11:28 -0400

    add prop types any to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
-function getDefaultLayout(page: ReactElement, pageProps) {
+function getDefaultLayout(page: ReactElement, pageProps: any) {
   return <Layout {...pageProps}>{page}</Layout>;
 }
 
