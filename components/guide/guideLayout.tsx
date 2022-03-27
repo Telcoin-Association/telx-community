d8db83c6d10bacf9596d504301263d62cd8b5878
Josh Worley <helloworley@gmail.com>
2022-03-27 18:43:22 -0400

    make content on education pages dynamic

--- components/guide/guideLayout.tsx
+++ components/guide/guideLayout.tsx
@@ -4,17 +4,24 @@ import GuideNav from "./guideNav";
 import GuideMain from "./guideMain";
 
 export interface GuideLayoutProps {
-  name?: string;
+  title?: string;
+  updated?: string;
+  content?: string;
 }
 
 export default function GuideLayout(props: GuideLayoutProps) {
+  const { title, content, updated } = props;
   
   return (
     <div className="guide-layout">
       <GuideBar />
       <div className="guide-layout-inner">
         <GuideNav />
-        <GuideMain />
+        <GuideMain 
+          title={title}
+          updated={updated}
+          content={content}
+        />
       </div>
     </div>
   )
--- components/guide/guideMain.tsx
+++ components/guide/guideMain.tsx
@@ -1,27 +1,21 @@
 import React from "react";
 
 export interface GuideMainProps {
-  name?: string;
+  title?: string;
+  updated?: string;
+  content?: string;
 }
 
 export default function GuideMain(props: GuideMainProps) {
   
+  const { title, content, updated } = props;
+
   return (
     <main className="guide-main">
       <div className="guide-main-inner">
-        <h1>What are AMM’s?</h1>
-        <h4>Updated March 27, 2022</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.
-          <br/><br/>
-          Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.
-          <br/><br/>
-          Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.
-          <br/><br/>
-          Praesent dapibus, neque id cursus faucibus, tortor neque egestas auguae, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.
-          <br/><br/>
-          Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.
-        </p>
+        <h1>{title}</h1>
+        <h4>Updated {updated}</h4>
+        <p>{content}</p>
       </div>
     </main>
   )
